c455efcb8b616d68e60a42eb24f59d2c6a3ad44f
zhonghua-wang <zhonghua.wang@outlook.com>
2016-11-15 00:37:13 +0800

    initial route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router'
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,13 @@ import { HeroesComponent } from './heroes/heroes.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
