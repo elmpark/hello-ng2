69cbe4ba4b23a6132df6f4ecdf44a68ee6a71a6e
zhonghua-wang <zhonghua.wang@outlook.com>
2016-11-21 20:33:16 +0800

    finish structural directive

--- src/app/structural-directive/structural-directive.module.ts
+++ src/app/structural-directive/structural-directive.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { StructuralDirectiveComponent } from './structural-directive.component';
 import {StructuralDirectiveRoutingModule} from "./structural-directive-routing.module";
 import { HeavyLoaderComponent } from './heavy-loader/heavy-loader.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     StructuralDirectiveRoutingModule
   ],
-  declarations: [StructuralDirectiveComponent, HeavyLoaderComponent]
+  declarations: [StructuralDirectiveComponent, HeavyLoaderComponent, UnlessDirective]
 })
 export class StructuralDirectiveModule { }
